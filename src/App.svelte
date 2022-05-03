45e19b01caefacc379ff820865ebbee50576116b
dglee <ldg6153@gmail.com>
2022-05-03 23:28:14 +0900

    UPDATE props name

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script>
 	export let name;
+	import Header from './components/header.svelte';
 </script>
 
 <main>
+	<Header/>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div></div>
 </main>
 
 <style>
