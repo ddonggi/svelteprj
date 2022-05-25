312beb29162da1ff57bebe11e6402a5717a8af5a
dglee <ldg6153@gmail.com>
2022-05-25 23:54:17 +0900

    REMOVE landing

---
+++ src/views/visitor/landing.svelte
@@ -0,0 +1,12 @@
+<script>
+    console.log('landing!')
+    import Header from '../../components/header.svelte'
+    import Section from '../../components/indexSection.svelte'
+    import Footer from '../../components/indexFooter.svelte'
+    export let kid;
+</script>
+
+<Header/>
+<Section/>
+<Footer kid="멀까"/>
+
