d446c545ed42a88efbd62bb29ee940aae0f28e57
dglee <ldg6153@gmail.com>
2022-06-05 23:26:21 +0900

    ADD | Desktop style

--- src/components/visitor/section.svelte
+++ src/components/visitor/section.svelte
@@ -1,6 +1,6 @@
 <script>
     import {push} from 'svelte-spa-router'
-    export let params;
+    export let params = {id:'dgleee'};
     export let name;
     export let age;
     export let src = 'https://i1.sndcdn.com/artworks-WkxGw85lhDbxVBfB-pe9z8Q-t500x500.jpg';
@@ -8,7 +8,7 @@
 </script>
 
 <section>
-<!--    <h1>Hello {name}, id : {params.id}</h1>-->
+    <h1>Hello {name}, id : {params.id}</h1>
     <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
     <img {src} alt="{name} image">
     <div> my age is {age}<br>
@@ -26,7 +26,13 @@
         font-weight: 100;
     }
 
+    /* desktop */
     @media (min-width: 640px) {
-
+      h1 {
+        color: blue;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+      }
     }
 </style>
