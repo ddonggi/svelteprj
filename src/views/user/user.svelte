1c52c342fdc7ad1de4fc6b5ea6849561bcabae4a
dglee <ldg6153@gmail.com>
2022-06-11 23:15:54 +0900

    FIX|user component name

--- src/views/user/user.svelte
+++ src/views/user/user.svelte
@@ -1,9 +1,9 @@
 <script>
     import Header from '../../components/header.svelte';
-    import VisitorSection from '../../components/user/section.svelte';
+    import UserSection from '../../components/user/section.svelte';
     import Footer from '../../components/user/footer.svelte';
     let sectionPackage = {
-        name:'기동이다',
+        name:'디케이',
         age:31,
         src:'https://upload.wikimedia.org/wikipedia/commons/f/f3/Meow_cat_-_Mdebona.jpg'
     }
@@ -11,6 +11,6 @@
     console.log('params id:',params.id)
 </script>
 <Header/>
-<VisitorSection {...sectionPackage}/>
+<UserSection {...sectionPackage}/>
 <Footer {params}/>
 
