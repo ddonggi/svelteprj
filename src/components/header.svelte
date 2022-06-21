35676b88de391b3c7bfc4d77c045ecae3e61d47d
dglee <ldg6153@gmail.com>
2022-06-22 00:05:34 +0900

    FIX | favicons in nav

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,19 +1,19 @@
 <script>
     import Fa from 'svelte-fa'
-    import { faFlag ,faGolfBall, faMessage} from '@fortawesome/free-solid-svg-icons'
+    import { faHouse ,faUser, faMessage, faCircleInfo, faLock, faSignOut} from '@fortawesome/free-solid-svg-icons'
 </script>
 <header>
     <nav class="navigation">
         <ul>
             <li>
                 <a href="#">
-                    <span class="icon"><Fa icon={faFlag} /></span>
+                    <span class="icon"><Fa icon={faHouse} /></span>
                     <span class="title">Home</span>
                 </a>
             </li>
             <li>
                 <a href="#">
-                    <span class="icon"><Fa icon={faGolfBall}/></span>
+                    <span class="icon"><Fa icon={faUser}/></span>
                     <span class="title">Profile</span>
                 </a>
             </li>
@@ -25,19 +25,19 @@
             </li>
             <li>
                 <a href="#">
-                    <span class="icon"></span>
+                    <span class="icon"><Fa icon={faCircleInfo}/></span>
                     <span class="title">Help</span>
                 </a>
             </li>
             <li>
                 <a href="#">
-                    <span class="icon"></span>
+                    <span class="icon"><Fa icon={faLock}/></span>
                     <span class="title">Password</span>
                 </a>
             </li>
             <li>
                 <a href="#">
-                    <span class="icon"></span>
+                    <span class="icon"><Fa icon={faSignOut}/></span>
                     <span class="title">Sign Out</span>
                 </a>
             </li>
