d8097e5a4cade524df40f629d357ea12d6b3ceef
dglee <ldg6153@gmail.com>
2022-06-23 23:54:00 +0900

    FIX | main page hover effect

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,21 @@
 <script>
 	import Router from 'svelte-spa-router'
 	import routes from './routers/routers.js'
-</script>
 
+	let hereKitty = false;
+
+	const handleMouseenter = () => hereKitty = true;
+	const handleMouseleave = () => hereKitty = false;
+</script>
+<svelte:body
+		on:mouseenter={handleMouseenter}
+		on:mouseleave={handleMouseleave}
+/>
+<img
+		class:curious={hereKitty}
+		alt="Kitten wants to know what's going on"
+		src="https://upload.wikimedia.org/wikipedia/commons/f/f3/Meow_cat_-_Mdebona.jpg"
+>
 <main>
 	<Router {routes}/>
 </main>
@@ -12,5 +25,22 @@
 		/*max-width: 240px;*/
 		width: 100%;
 		margin: 0 auto;
+	}
+	img {
+		position: absolute;
+		left: 0;
+		bottom: -60px;
+		transform: translate(-80%, 0) rotate(-30deg);
+		transform-origin: 100% 100%;
+		transition: transform 0.4s;
+	}
+
+	.curious {
+		transform: translate(-15%, 0) rotate(0deg);
+	}
+
+	:global(body) {
+		overflow: hidden;
+		padding:0;
 	}
 </style>
